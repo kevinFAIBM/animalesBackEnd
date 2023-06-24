5db7b4c6b08794d8687b759e40d18d55744d38a6
kevinFAIBM <131728292+kevinFAIBM@users.noreply.github.com>
2023-06-24 17:49:50 -0600

    First commit

---
+++ src/test/java/com/animalesBackEnd/animalesBackEnd/AnimalesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalesBackEnd.animalesBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
