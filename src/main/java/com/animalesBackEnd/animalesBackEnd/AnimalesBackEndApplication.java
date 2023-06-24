141feef2b2a8cdced020471a278567749e238802
kevinFAIBM <131728292+kevinFAIBM@users.noreply.github.com>
2023-06-24 17:57:15 -0600

    first maven commit

---
+++ src/main/java/com/animalesBackEnd/animalesBackEnd/AnimalesBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.animalesBackEnd.animalesBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalesBackEndApplication.class, args);
+	}
+
+}
